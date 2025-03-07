b42da019cc2c0262b7d49d4055fdf1f84053340c
Baldosano, Arthur Jr. V. <202480019@psu.palawan.edu.ph>
2025-03-07 22:36:51 +0530

    Update reviewer.js

--- reviewer.js
+++ reviewer.js
@@ -3,12 +3,19 @@ function scrollToTop() {
 }
 function toggleSidebar() {
         let sidebar = document.getElementById("sidebar");
-        let content = document.querySelector(".content");
-        sidebar.classList.toggle("collapsed");
+        let toggleBtn = document.getElementById("toggleBtn");
+        let gridContainer = document.querySelector(".grid-container");
+        let ulo = document.querySelector(".ulo");
         
+        sidebar.classList.toggle("collapsed");
+
         if (sidebar.classList.contains("collapsed")) {
-            content.style.marginLeft = "0";
+            gridContainer.style.marginLeft = "0";
+            ulo.style.marginLeft = "20px";
+            toggleBtn.style.left = "10px";
         } else {
-            content.style.marginLeft = "250px";
+            gridContainer.style.marginLeft = "250px";
+            ulo.style.marginLeft = "270px";
+            toggleBtn.style.left = "210px";
         }
-        }
+    }
