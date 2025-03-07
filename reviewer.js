db79e480dc75e736623cbcb3a30c61b82add1e94
Baldosano, Arthur Jr. V. <202480019@psu.palawan.edu.ph>
2025-03-07 22:47:54 +0530

    Update reviewer.js

--- reviewer.js
+++ reviewer.js
@@ -2,20 +2,20 @@ function scrollToTop() {
         window.scrollTo({ top: 0, behavior: "smooth" });
 }
 function toggleSidebar() {
-        let sidebar = document.getElementById("sidebar");
-        let toggleBtn = document.getElementById("toggleBtn");
-        let gridContainer = document.querySelector(".grid-container");
-        let ulo = document.querySelector(".ulo");
-        
-        sidebar.classList.toggle("collapsed");
+    let sidebar = document.getElementById("sidebar");
+    let toggleBtn = document.getElementById("toggleBtn");
+    let gridContainer = document.querySelector(".grid-container");
+    let ulo = document.querySelector(".ulo");
 
-        if (sidebar.classList.contains("collapsed")) {
-            gridContainer.style.marginLeft = "0";
-            ulo.style.marginLeft = "20px";
-            toggleBtn.style.left = "10px";
-        } else {
-            gridContainer.style.marginLeft = "250px";
-            ulo.style.marginLeft = "270px";
-            toggleBtn.style.left = "210px";
-        }
+    sidebar.classList.toggle("collapsed");
+
+    if (sidebar.classList.contains("collapsed")) {
+        gridContainer.style.marginLeft = "0";
+        ulo.style.marginLeft = "20px";
+        toggleBtn.style.left = "10px";
+    } else {
+        gridContainer.style.marginLeft = "200px"; /* Match the sidebar width */
+        ulo.style.marginLeft = "220px";
+        toggleBtn.style.left = "210px";
     }
+}
